9715fa315fda3554a04c77deca6ccf2e5d4ca4db
Linda <lihuiling987@gmail.com>
2020-07-26 12:03:14 +1200

    Update text

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>MSA 2020.</p>
+        <p>MSA 2020 - This is develop branch.</p>
         <a
           className="App-link"
           href="https://msa.azurewebsites.net/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          MSA try
+          MSA try it out
         </a>
       </header>
     </div>
